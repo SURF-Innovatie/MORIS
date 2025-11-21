b47aee1f85697edd5e4c96a4343d23891e54700d
Geert Haans <geert.haans@gmail.com>
2025-11-21 10:58:04 +0100

    feat(user): make id unique in db

--- apps/backend/ent/schema/user.go
+++ apps/backend/ent/schema/user.go
@@ -14,7 +14,7 @@ type User struct {
 // Fields of the User.
 func (User) Fields() []ent.Field {
 	return []ent.Field{
-		field.UUID("id", uuid.UUID{}).Default(uuid.New).Immutable(),
+		field.UUID("id", uuid.UUID{}).Default(uuid.New).Immutable().Unique(),
 		field.String("name").NotEmpty(),
 		field.String("email").Unique(),
 		field.String("password").NotEmpty().Sensitive(), // Sensitive prevents it from being logged
