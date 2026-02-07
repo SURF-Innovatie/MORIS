3d387af297d27335144b98d34cf427b94d58a968
Ben Stokmans <benstokmans@hotmail.nl>
2026-02-07 14:37:43 +0100

    fix: remove redundant wiring

--- apps/backend/internal/infra/eventdispatch/di/package.go
+++ apps/backend/internal/infra/eventdispatch/di/package.go
@@ -12,23 +12,17 @@ var Package = do.Package(
 )
 
 func provideEventPublisher(i do.Injector) (event.Publisher, error) {
-	projHandler := do.MustInvoke[*events.ProjectEventNotificationHandler](i)
-	approvalHandler := do.MustInvoke[*events.ApprovalRequestNotificationHandler](i)
 	policyHandler := do.MustInvoke[*events.Handler](i)
 	execHandler := do.MustInvoke[*events.PolicyExecutionHandler](i)
 
 	notificationHandlers := []eventdispatch.NotificationHandler{
-		projHandler,
-		approvalHandler,
 		policyHandler,
 		execHandler,
 	}
 
-	statusHandler := do.MustInvoke[*events.StatusUpdateNotificationHandler](i)
 	cacheHandler := do.MustInvoke[*events.CacheRefreshHandler](i)
 
 	statusChangeHandlers := []eventdispatch.StatusChangeHandler{
-		statusHandler.Handle,
 		cacheHandler.Handle,
 	}
 
